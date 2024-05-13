c7d8088f57c54ea2fedac11ff9981207f4141021
Dell <mjazzazi98@gmail.com>
2024-05-13 13:35:09 +0300

    first commit

---
+++ src/test/java/com/ClusteredData/ClusteredData/ClusteredDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClusteredData.ClusteredData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusteredDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
